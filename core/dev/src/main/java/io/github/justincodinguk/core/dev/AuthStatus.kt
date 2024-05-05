722ab175592bc24d42a759517730c1b0b2bc1509
JustinW <youdontneedtoknowmyemail@google.com>
2024-05-05 19:12:51 +0100

    Move AuthStatus to dev module

--- core/dev/src/main/java/io/github/justincodinguk/core/dev/AuthStatus.kt
+++ core/dev/src/main/java/io/github/justincodinguk/core/dev/AuthStatus.kt
@@ -1,9 +1,11 @@
-package io.github.justincodinguk.core.firebase.auth
+package io.github.justincodinguk.core.dev
 
 import io.github.justincodinguk.core.model.User
 
 sealed class AuthStatus(val user: User? = null) {
     class Authenticated(val result: User) : AuthStatus(result)
     data object Unauthenticated : AuthStatus()
+    data object Error : AuthStatus()
     data object Loading : AuthStatus()
+    data object VerificationNeeded : AuthStatus()
 }
